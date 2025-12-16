b77ac5b470dc1dde4b59d4a80ca826adb56110e2
piusrepo <152050670+piusrepo@users.noreply.github.com>
2025-12-16 08:23:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(flash());
 app.set('view engine', 'ejs');
 
 // DB config
-const db = 'mongodb+srv://marcelpolocha1:081358pius@cluster0.f9a85hv.mongodb.net/piuscrypto';
+const db = 'mongodb+srv://marcelpolocha1:081358pius@cluster0.f9a85hv.mongodb.net/ globalfleixtyipests';
 mongoose.connect(db)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
